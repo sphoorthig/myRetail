c3d9cd1165c2ccc25d307a0ca870dace8cbd7015
Sphoorthi Gaddam <sphoorthigaddam@gmail.com>
2020-03-21 12:54:07 -0400

    Project initialized

---
+++ src/test/java/com/target/myRetail/myRetail/MyRetailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.target.myRetail.myRetail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRetailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
