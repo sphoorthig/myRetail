b7e06e5fc41f8318212495250c95624a6319b067
Sphoorthi Gaddam <sphoorthigaddam@gmail.com>
2020-03-29 15:04:07 -0400

    Added enable swagger on application

--- src/main/java/com/target/myRetail/MyRetailApplication.java
+++ src/main/java/com/target/myRetail/MyRetailApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableMongoRepositories
+@EnableSwagger2
 public class MyRetailApplication {
 
 	public static void main(String[] args) {
