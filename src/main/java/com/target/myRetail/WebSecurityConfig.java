bba3101046d06e4d563f4a943f827fe221fce160
Sphoorthi Gaddam <sphoorthigaddam@gmail.com>
2020-03-29 02:00:20 -0400

    Added default web security and disabled csrf

---
+++ src/main/java/com/target/myRetail/WebSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.target.myRetail;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable();
+    http.cors();
+    http.authorizeRequests().anyRequest().fullyAuthenticated();
+    http.httpBasic();
+  }
+}
